a47ded6f28bc6d79bdcb46ab09c09e1286314105
Anthony Wong <antawck@gmail.com>
2021-03-24 17:49:33 +0800

    custom/original column added. table for emojis in a guild added

--- schema.py
+++ schema.py
@@ -4,5 +4,12 @@ CREATE TABLE emoji_usages
 	channel BIGINT UNSIGNED,
 	user BIGINT UNSIGNED,
 	emoji VARCHAR(255),
+	type ENUM('original', 'custom'),
 	time TIMESTAMP
+);
+
+CREATE TABLE emojis
+(
+	guild BIGINT UNSIGNED,
+	emojiID BIGINT UNSIGNED
 );
