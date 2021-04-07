111c25f0b4e00dd4795b1aeee5b7e31a24e3c7a2
Anthony Wong <antawck@gmail.com>
2021-04-08 03:24:08 +0800

    param parsing, emoji query for custom and original

--- db.py
+++ db.py
@@ -45,11 +45,60 @@ class db:
             emojiType = 'original'
             if(isinstance(emoji, int) or emoji.isnumeric()):
                 emojiType = 'custom'
-            c.execute(f"INSERT INTO emoji_usages (channel, user, emoji, type) VALUES ({server}, {channel}, {user}, '{emoji}', '{emojiType}')")
+            c.execute(f"INSERT INTO emoji_usages (channel, user, emoji, type) VALUES ({channel}, {user}, '{emoji}', '{emojiType}')")
 
         db.getdb().commit()
 
         return
+
+    def getEmojiUsages(target, timeOffsetString, orientation, count, relevantCustomEmojiIDs):
+        c = db.getdb().cursor()
+
+        c.execute("CREATE TEMPORARY TABLE allEmojis (emoji VARCHAR(255), count INT UNSIGNED);")
+
+        for emoji in relevantCustomEmojiIDs:
+            c.execute(f"INSERT INTO allEmojis VALUES ({emoji}, 0);")
+
+        orderSort = {
+            "top" : "DESC",
+            "bottom" : "ASC"
+        }
+
+        startTime = 0
+
+        if(timeOffsetString != "all"):
+            timeOffset = convertToSeconds(timeOffsetString)
+            startTime = int(time.time()) - timeOffset
+
+        # JFC WTF IS THIS QUERY
+        query = f"""
+        SELECT emoji, sum(count) as count FROM (
+            SELECT usedEmojis.emoji, usedEmojis.count FROM (
+                SELECT combined.emoji as emoji, count(combined.emoji) as count FROM (
+                    SELECT emoji FROM emoji_usages WHERE type = "original" AND {target[0]} = {target[1]} AND UNIX_TIMESTAMP(time) > {startTime}
+
+                    UNION ALL
+
+                    SELECT emoji FROM emoji_usages WHERE type = "custom" AND {target[0]} = {target[1]} AND UNIX_TIMESTAMP(time) > {startTime} AND emoji IN ({",".join(map(str,relevantCustomEmojiIDs))})
+                ) AS combined
+
+                GROUP BY emoji
+            ) as usedEmojis 
+            
+            UNION ALL
+
+            SELECT emoji, count FROM allEmojis
+        ) 
+        x GROUP BY emoji ORDER BY count {orderSort[orientation]} LIMIT {count}
+        """
+
+        c.execute(query)
+
+        data = c.fetchall()
+
+        c.execute("DROP TEMPORARY TABLE allEmojis;")
+
+        return data
 
 # Helper function, if more comes, move to different file
 def convertToSeconds(string):
@@ -65,3 +114,4 @@ def convertToSeconds(string):
     }
 
     return int(amount) * intervalSeconds[interval]
+
--- main.py
+++ main.py
@@ -77,11 +77,73 @@ def isChannelTag(text):
 def isUserTag(text):
     return re.search("^<@!?\d+>$", text)
 
+def isTime(text):
+    # ugly type mixing
+    return (time == "all") or re.search("^\d+[dwmy]$", value)
+
+def discTagToID(tag):
+    # Could user discord's converter here but why? Non critical stuff + would need to await for stuff
+    charsToBeRemoved = ["<", "@", "!", "#", ">"]
+
+    for c in charsToBeRemoved:
+        tag = tag.replace(c, "")
+
+    return tag
+
 @bot.command(aliases = ['emoji', 'emojiUse', 'e'])
 async def emoji_report(msg, *, args = ""):
+
+    validOptions = {
+        "top" : str.isnumeric,
+        "bottom" : str.isnumeric,
+        "target" : isTarget,
+        "time" : isTime
+    }
+
+    orientationKeys = ["top", "bottom"]
+
+    selectedOptions = {
+        "time" : "1m",
+        "target" : ("user", msg.author.id)
+    }
+
+    for match in re.finditer("-(\s*\w+)([^-]*)", args):
+
+        option = match.group(1).strip()
+        param = match.group(2).strip()
+
+        if(not option in validOptions):
+            await msg.channel.send(f"**{option}** is not a valid option for this command.")
+            return
+
+        if(not param):
+            await msg.channel.send(f"No parameter was given for the option **{option}**")
+            return
+
+        validatorData = validOptions[option](param)
+
+        if(not validatorData):
+            await msg.channel.send(f"**{param}** is not a valid parameter for the option **{option}**.")
+            return
+
+        if(option == "target"): 
+            if(validatorData == "server"):
+                selectedOptions[option] = ("server", "server")
+            else:
+                selectedOptions[option] = (validatorData, discTagToID(param))
+
+        else:
+            selectedOptions[option] = param
+
+    emojiData = {}
+
+    for key in orientationKeys:
+        if(key in selectedOptions):
+            emojiData[key] = db.getEmojiUsages(selectedOptions["target"], selectedOptions["time"], key, selectedOptions[key], [emoji.id for emoji in msg.guild.emojis])
+
     
 
-
+    
 
 @bot.event
 async def on_command_error(ctx, error):
